94f532a7a36363bc61a012c4ff6653767fff061b
MohdAjmalRaza <ajmalbly27@gmail.com>
2024-09-07 16:27:38 +0530

    Ep09: custom hook and Lazy-Loading

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   //Local State Variable - Super powerful variable
@@ -37,6 +38,15 @@ const Body = () => {
       json?.data?.cards[0]?.groupedCard?.cardGroupMap?.RESTAURANT?.cards
     );
   };
+
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you're offline!! Please Check Your internet connection.
+      </h1>
+    );
 
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
