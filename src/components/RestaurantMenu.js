34d9d8a3467f8a191b2b75309cd81fd39ea7305a
MohdAjmalRaza <ajmalbly27@gmail.com>
2024-09-30 01:42:25 +0530

    fix: accordion behaviour

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -39,7 +39,13 @@ const RestaurantMenu = () => {
           key={category?.card?.card.title}
           data={category?.card?.card}
           showItems={index === showIndex ? true : false}
-          setShowIndex={() => setShowIndex(index)}
+          setShowIndex={() => {
+            if (index === showIndex) {
+              setShowIndex(-1);
+            } else {
+              setShowIndex(index);
+            }
+          }}
         />
       ))}
     </div>
